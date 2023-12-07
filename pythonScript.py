3afe68844f88cff66691f2c8e0b898f043b096d8
mgsanthosh <mgsanthosh553@gmail.com>
2023-12-07 14:39:23 +0530

    finished running the script

---
+++ pythonScript.py
@@ -0,0 +1,20 @@
+import sys
+
+def transform_input(input_string):
+    # Extract name from input string
+    start_index = input_string.find('<') + 1
+    end_index = input_string.find('>', start_index)
+    name = input_string[start_index:end_index]
+
+    # Transform input
+    transformed_output = f'Python Printing: {name}'
+    return transformed_output
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python pythonScript.py "<input_string>"')
+        sys.exit(1)
+
+    input_string = sys.argv[1]
+    result = transform_input(input_string)
+    print(result)
